7a40e7dbccd76920dff4614e44e274d6ceb38f30
JamesGorsuch <gorsuchjames80@gmail.com>
2025-07-14 23:12:55 -0500

    Add files via upload

---
+++ NeuralNetworkTest.py
@@ -0,0 +1,110 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data, vertical_data
+
+nnfs.init()
+
+#X = [[1,2,3,2.5],[2,5,-1,2],[-1.5,2.5,3,-0.8]]
+
+X, y = vertical_data(samples=100, classes=3)
+
+class Layer_Dense:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1,n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+    
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+        
+class Activation_softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=1))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=1)
+        self.output = probabilities
+        
+class Loss:
+    def calculate(self,output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+class Loss_CategoricalCrossentrophy(Loss):
+    def forward(self, y_pred, y_true):
+        sample = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7) #basically 0, 1
+        
+        if len(y_true.shape) == 1: #if scaler values
+            correct_confidences = y_pred_clipped[range(sample), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+            
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+            
+
+
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_softmax()
+
+loss_function = Loss_CategoricalCrossentrophy()
+
+lowest_loss = 9999999
+best_dense1_weights = dense1.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense2_weights = dense2.weights.copy()
+best_dense2_biases = dense2.biases.copy()
+
+for iteration in range(100000):
+    
+    dense1.weights += 0.05 * np.random.randn(2,3)
+    dense1.biases += 0.05 * np.random.randn(1,3)
+    dense2.weights += 0.05 * np.random.randn(3,3)
+    dense2.biases += 0.05 * np.random.randn(1,3)
+    
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+    
+    loss = loss_function.calculate(activation2.output, y)
+    
+    predictions = np.argmax(activation2.output, axis=1)
+    accuracy = np.mean(predictions==y)
+    
+    if loss < lowest_loss:
+        best_dense1_weights = dense1.weights.copy()
+        best_dense1_biases = dense1.biases.copy()
+        best_dense2_weights = dense2.weights.copy()
+        best_dense2_biases = dense2.biases.copy()
+        lowest_loss = loss
+    else:
+        dense1.weights = best_dense1_weights.copy()
+        dense1.biases = best_dense1_biases.copy()
+        dense2.weights = best_dense2_weights.copy()
+        dense2.biases = best_dense2_biases.copy()
+
+
+
+
+
+
+
+
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
+
+
+loss = loss_function.calculate(activation2.output, y)
+
+print("Loss: ", loss)
