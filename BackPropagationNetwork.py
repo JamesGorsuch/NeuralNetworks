004cc8edac05d60889d9ad1840cead7fbdbad362
JamesGorsuch <gorsuchjames80@gmail.com>
2025-07-17 21:51:02 -0500

    Add files via upload

--- BackPropagationNetwork.py
+++ BackPropagationNetwork.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 nnfs.init()
 
 #The vertical data is just three groups of points each with their own class, this is basic data but easy to understand for the network
-X, y = vertical_data(samples=100, classes=3)
+X, y = spiral_data(samples=100, classes=3)
 
 
 class Layer_Dense:
@@ -16,8 +16,15 @@ class Layer_Dense:
     def __init__(self,n_inputs,n_neurons):
         self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1,n_neurons))
+        
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.dot(inputs, self.weights) + self.biases
+        
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
 
 class Activation_ReLU:
@@ -26,7 +33,12 @@ class Activation_ReLU:
     gets rid of negatives
     """
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
+        
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
 
 
 class Activation_softmax:
@@ -37,6 +49,16 @@ class Activation_softmax:
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
+        
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+            
+        self.dinputs = dvalues.copy()
+        self.dinputs[range(samples), y_true] -= 1
+        self.dinputs = self.dinputs / samples
 
 #how far from the expected value
 class Loss:
@@ -102,7 +124,19 @@ def ContinueToInput():
     #print the probabilities
     print(activation2.output[0])
     
+#pass everythjng through the program
+def ForwardPass():
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
     
+def BackwardPass():
+    activation2.backward(activation2.output, y)
+    dense2.backward(activation2.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1.backward(activation1.dinputs)
+
 #Build and connect the actual layers of the network
 dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
@@ -117,49 +151,50 @@ best_dense1_biases = dense1.biases.copy()
 best_dense2_weights = dense2.weights.copy()
 best_dense2_biases = dense2.biases.copy()
 
-#train the model using random weight mutations with the lowest loss
+#lists for plot
 lossList = []
 iList = []
-for iteration in range(100000):
-    #mutation!
-    dense1.weights += 0.05 * np.random.randn(2,3)
-    dense1.biases += 0.05 * np.random.randn(1,3)
-    dense2.weights += 0.05 * np.random.randn(3,3)
-    dense2.biases += 0.05 * np.random.randn(1,3)
+
+#Training parameters
+learning_rate = 0.1
+iterations = 100000
+
+#train the model using random weight mutations with the lowest loss
+for iteration in range(iterations):
+    ForwardPass()
     
-    dense1.forward(X)
-    activation1.forward(dense1.output)
-    dense2.forward(activation1.output)
-    activation2.forward(dense2.output)
-    
+    #Calculate loss
     loss = loss_function.calculate(activation2.output, y)
     
+    BackwardPass()
     
-    #keep the mutation if its better
+    # Update weights and biases
+    dense1.weights -= learning_rate * dense1.dweights
+    dense1.biases -= learning_rate * dense1.dbiases
+    dense2.weights -= learning_rate * dense2.dweights
+    dense2.biases -= learning_rate * dense2.dbiases
+
+    # Save best model
     if loss < lowest_loss:
+        lowest_loss = loss
         best_dense1_weights = dense1.weights.copy()
         best_dense1_biases = dense1.biases.copy()
         best_dense2_weights = dense2.weights.copy()
         best_dense2_biases = dense2.biases.copy()
-        lowest_loss = loss
-        
-        #plot Loss x Iteration for visualization
+    
+    if iteration % 1000 == 0:
         lossList.append(loss)
         iList.append(iteration)
-    else:
-        dense1.weights = best_dense1_weights.copy()
-        dense1.biases = best_dense1_biases.copy()
-        dense2.weights = best_dense2_weights.copy()
-        dense2.biases = best_dense2_biases.copy()
+        print(f"Iteration: {iteration} Loss: {loss}")
+        
+    
+    
+    
 
 plt.plot(lossList,iList)
 
 
-dense1.forward(X)
-activation1.forward(dense1.output)
-
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
+ForwardPass()
 
 #print the top 5, each # is probability of that class ([prob of class 0   prob of class 1    prob of class 2])
 print(activation2.output[:5])
--- DataSetShower.py
+++ DataSetShower.py
@@ -5,7 +5,7 @@ from nnfs.datasets import vertical_data, spiral_data
 #assist me with creating a basic network
 nnfs.init()
 
-X, y = vertical_data(samples=100, classes=3)
+X, y = spiral_data(samples=100, classes=3)
 
 plt.scatter(X[:,0], X[:,1], c=y, s=40, cmap="brg")
 plt.show()
